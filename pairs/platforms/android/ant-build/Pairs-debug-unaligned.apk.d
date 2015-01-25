80e950dc45914c286a44b550ac8b28a47125078c
James Tease <jamestease@gmail.com>
2015-01-25 11:55:40 +0000

    actually working apk

---
+++ pairs/platforms/android/ant-build/Pairs-debug-unaligned.apk.d
@@ -0,0 +1,7 @@
+E:\1gam\memory\pairs\platforms\android\ant-build\Pairs-debug-unaligned.apk : \
+E:\1gam\memory\pairs\platforms\android\ant-build\Pairs.ap_ \
+E:\1gam\memory\pairs\platforms\android\ant-build\classes.dex \
+E:\1gam\memory\pairs\platforms\android\CordovaLib\ant-build\classes.jar \
+E:\1gam\memory\pairs\platforms\android\libs\android-support-v4.jar \
+E:\1gam\memory\pairs\platforms\android\libs\google-play-services.jar \
+E:\1gam\memory\pairs\platforms\android\libs\cordova-generic-ad.jar \
